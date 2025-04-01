6be73d1d4445882d172ea3bf30477ec05a87b07b
Exauce Bodi <exaucebodi4@gmail.com>
2025-04-01 22:50:17 +0200

    Le commit initial

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:blog_mobile/pages/list-artcle/listArticle.dart';
+import 'package:blog_mobile/pages/login/LoginPage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+void main(){
+
+  runApp(ProviderScope(child: Myapp()));
+
+}
+
+class Myapp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+
+}
